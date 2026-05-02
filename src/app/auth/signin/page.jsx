f5bb9fa1eaf961ae344229107a8d4ce013bdcf5e
Moziful Haque <mhmoni2310@gmail.com>
2026-05-03 01:54:01 +0600

    Added Sign In And Sign Up Page From DaisyUI

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,38 @@
+const SignInPage = () => {
+  return (
+    <div className="flex h-[90vh] justify-center items-center">
+      <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input validator"
+            placeholder="Email"
+            required
+          />
+          <p className="validator-hint hidden">Required</p>
+        </fieldset>
+
+        <label className="fieldset">
+          <span className="label">Password</span>
+          <input
+            type="password"
+            className="input validator"
+            placeholder="Password"
+            required
+          />
+          <span className="validator-hint hidden">Required</span>
+        </label>
+
+        <button className="btn btn-neutral mt-4" type="submit">
+          Login
+        </button>
+        <button className="btn btn-ghost mt-1" type="reset">
+          Reset
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignInPage;
